c36bd9de895a4a053a8ef369e2ff84c20272bb94
fernando-rosa-wmw <fernando.rosa@wmw.com.br>
2024-07-17 18:04:22 -0300

    add index on transaction schema.

--- src/components/transaction/infra/mongo/entity/mongo-transaction.ts
+++ src/components/transaction/infra/mongo/entity/mongo-transaction.ts
@@ -2,10 +2,12 @@ import { Schema, model } from 'mongoose';
 
 const schema = new Schema({
     userId: String,
-    sourceAccountId: String,
-    destinyAccountId: String,
+    sourceAccountId: { type: String, index: true },
+    destinyAccountId: { type: String, index: true },
     type: String,
     value: Number,
 });
+
+schema.index({ sourceAccountId: 1, destinyAccountId: 1 });
 
 export const MongoTransaction = model('Transaction', schema);
